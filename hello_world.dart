9567118effb62273cbb44ce8954ca9a60c23116a
齐磊 <qilei@qileideMacBook-Pro.local>
2020-08-10 16:23:23 +0800

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("hello world!");
+}
+
+// 参考 https://www.jianshu.com/p/b9c72de1de52
+
+// 运行方式1：运行命令  dart hello_world.dart  输出 hello world!
+
+// 运行方式2：安装插件： code-runner，右键 Run Code
